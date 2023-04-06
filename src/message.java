53fc34bac4edc0f8826b083fc86fd03a3ca6e8af
AnastasiiaOzerska <a.ozolova@gmail.com>
2023-04-06 13:42:35 +0300

    Initial commit

--- src/message.java
+++ src/message.java
@@ -4,11 +4,16 @@ public class message {
     public static void main(String[] args) {
         String militaryCommissarMessage = " , visit the draft board";
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.next();
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Enter your name");
+            String name = scanner.next();
+            System.out.println("Enter your age");
         int age = scanner.nextInt();
         if(age>=18 && age<=26){
             System.out.println(name + militaryCommissarMessage);
+        }else {
+            System.out.println("You are not the right age");
         }
 
     }
-}
+}}
